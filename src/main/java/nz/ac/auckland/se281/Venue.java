0f78378e00ab60d10b27cf7295c06f54762bce7b
Puthikunkim <jerryputhikunkim@gmail.com>
2024-03-23 13:08:43 +1300

    Reformatted code and did code styling

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -6,27 +6,28 @@ public class Venue {
   private String capacityInput;
   private String hireFeeInput;
 
+  // Constructor for the Venue class
   public Venue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     this.venueName = venueName;
     this.venueCode = venueCode;
     this.capacityInput = capacityInput;
     this.hireFeeInput = hireFeeInput;
   }
-  // Create getter methods for the values
+
+  // Getter methods for the values
   public String getVenueName() {
     return venueName;
   }
-  // Create getter methods for the values
+
   public String getVenueCode() {
     return venueCode;
   }
-  // Create getter methods for the values
+
   public String getCapacityInput() {
     return capacityInput;
   }
-  // Create getter methods for the values
+
   public String getHireFeeInput() {
     return hireFeeInput;
   }
-
 }
--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -11,11 +11,13 @@ public class VenueHireSystem {
   public VenueHireSystem() {}
 
   public void printVenues() {
-    // No venues
+
+    // Check for no venues.
     if (venueList.isEmpty()) {
       String noVenuesMessage = MessageCli.NO_VENUES.getMessage();
       System.out.println(noVenuesMessage);
-    } else if (venueList.size() == 1) { // One venue
+    } else if (venueList.size() == 1) {
+      // Check for one venue.
       String numberVenuesMessage = MessageCli.NUMBER_VENUES.getMessage("is", "one", "");
       System.out.println(numberVenuesMessage);
       String venueEntryMessage =
@@ -26,7 +28,8 @@ public class VenueHireSystem {
               venueList.get(0).getHireFeeInput(),
               "");
       System.out.println(venueEntryMessage);
-    } else if (venueList.size() > 1 && venueList.size() < 10) { // Under ten venues
+    } else if (venueList.size() > 1 && venueList.size() < 10) {
+      // Check for under ten venues.
       int number = venueList.size();
       String numberConverted = "";
       switch (number) {
@@ -69,7 +72,8 @@ public class VenueHireSystem {
                 "");
         System.out.println(venueEntryMessage);
       }
-    } else { // More than 10 venues
+    } else {
+      // Check for more than 10 venues.
       String numberVenuesMessage =
           MessageCli.NUMBER_VENUES.getMessage("are", String.valueOf(venueList.size()), "s");
       System.out.println(numberVenuesMessage);
@@ -90,14 +94,14 @@ public class VenueHireSystem {
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     boolean validity = true;
-    // venueName check
+    // venueName check.
     if (venueName.trim().isEmpty()) {
       validity = false;
       String venueUnsuccessfulMessage = MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.getMessage();
       System.out.println(venueUnsuccessfulMessage);
     }
 
-    // venueCode check
+    // venueCode check.
     for (int i = 0; i < venueList.size(); i++) {
       if (venueList.get(i).getVenueCode().equals(venueCode)) {
         validity = false;
@@ -109,7 +113,7 @@ public class VenueHireSystem {
       }
     }
 
-    // capacityInput number check
+    // capacityInput number check.
     try {
       int intCapacity = Integer.parseInt(capacityInput);
       if (intCapacity < 1) {
@@ -125,7 +129,7 @@ public class VenueHireSystem {
       System.out.println(venueUnsuccessfulMessage);
     }
 
-    // hireFeeInput number check
+    // hireFeeInput number check.
     try {
       int intHireFee = Integer.parseInt(hireFeeInput);
       if (intHireFee < 1) {
@@ -141,7 +145,7 @@ public class VenueHireSystem {
       System.out.println(venueUnsuccessfulMessage);
     }
 
-    // Success
+    // Successful venue created.
     if (validity) {
       String venueSuccessfullyCreatedMessage =
           MessageCli.VENUE_SUCCESSFULLY_CREATED.getMessage(venueName, venueCode);
